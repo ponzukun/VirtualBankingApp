265b30fdda8e34d7e7bdb8236aabba128c9eea4d
ponzukun <p1o1n1z1u@gmail.com>
2022-04-16 18:37:08 +0900

    [add] class BankAccount, func getRandomInteger

---
+++ js/bankaccount.js
@@ -0,0 +1,15 @@
+export class BankAccount {
+    constructor(firstName, lastName, email, type, accountNumber, money) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.type = type;
+        this.accountNumber = accountNumber;
+        this.money = money;
+        this.initialDeposit = money;
+    }
+
+    getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
+import { BankAccount } from "./bankaccount.js";
+
 const config = {
     initialForm : document.getElementById("initial-form"),
     bankPage : document.getElementById("bank-page")
 }
 
-// console.log(config.initialForm);
-// console.log(config.bankPage);
+function getRandomInteger(min, max) {
+    if(max < min) return null;
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+let user1 = new BankAccount("Elisa", "Jones", "elisa.jones@gmail.com", "checking", getRandomInteger(1,1000), "30");
+console.log(user1);
