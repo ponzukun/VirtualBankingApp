1212b87db1652f2611bac0648649a6ff78b09ff3
ponzukun <p1o1n1z1u@gmail.com>
2022-04-16 20:25:57 +0900

    [add func] billInputSelector, backNextBtn

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ import { BankAccount } from "./bankaccount.js";
 const config = {
     initialForm : document.getElementById("initial-form"),
     bankForm : document.getElementById("bank-form"),
-    bankPage : document.getElementById("bank-page")
+    bankPage : document.getElementById("bank-page"),
+    withdrawPage : document.getElementById("withdraw-page")
 }
 
 function bankFormSubmit(event) {
@@ -45,14 +46,14 @@ function mainBankPage(userBankAccount) {
         <div class="py-1">Your Name: ${userBankAccount.getFullName()}</div>
         <div class="py-1">Your Back ID: ${userBankAccount.accountNumber}</div>
         <div class="py-1">Your First Deposit: ${userBankAccount.initialDeposit}</div>
-    `
+    `;
 
     let balanceCon = document.createElement("div");
     balanceCon.classList.add("bg-danger", "d-flex", "flex-wrap", "py-1", "py-md-2");
     balanceCon.innerHTML = `
         <div class="col-md-7 col-12 rem2">Available Balance</div>
         <div class="col-md-5 col-12 rem2">$${userBankAccount.money}</div>
-    `
+    `;
 
     let menuCon = document.createElement("div");
     menuCon.classList.add("my-3", "d-flex", "justify-content-center", "flex-wrap", "col-12");
@@ -75,10 +76,12 @@ function mainBankPage(userBankAccount) {
                 <i class="fa-3x fas fa-home"></i>
             </div>
         </div>
-    `
+    `;
 
     menuCon.querySelectorAll("#withdrawBtn").item(0).addEventListener("click", function(){
-        window.alert("withdraw");
+        config.bankPage.classList.add("d-none");
+        config.withdrawPage.classList.remove("d-none");
+        config.withdrawPage.append(billInputSelector("Please Enter The Withdrawal Amount"));
     });
     menuCon.querySelectorAll("#depositBtn").item(0).addEventListener("click", function(){
         window.alert("deposit");
@@ -88,5 +91,83 @@ function mainBankPage(userBankAccount) {
     });
 
     container.append(infoCon, balanceCon, menuCon);
+    return container;
+}
+
+function billInputSelector(title) {
+    let container = document.createElement("div");
+    container.innerHTML = `
+        <h2>${title}</h2>
+        <div class="mt-3">
+            <div class="mb-2 d-flex justify-content-between">
+                <div class="col-3 pl-0">
+                    <label for="100-dollars" class="col-12">$100</label>
+                </div>
+                <div class="col-8 pr-0">
+                    <input class="col-12" type="number" id="100-dollars" name="100-dollars" min="0" value="">
+                </div>
+            </div>
+            <div class="mb-2 d-flex justify-content-between">
+                <div class="col-3 pl-0">
+                    <label for="50-dollars" class="col-12">$50</label>
+                </div>
+                <div class="col-8 pr-0">
+                    <input class="col-12" type="number" id="50-dollars" name="50-dollars" min="0" value="">
+                </div>
+            </div>
+            <div class="mb-2 d-flex justify-content-between">
+                <div class="col-3 pl-0">
+                    <label for="20-dollars" class="col-12">$20</label>
+                </div>
+                <div class="col-8 pr-0">
+                    <input class="col-12" type="number" id="20-dollars" name="20-dollars" min="0" value="">
+                </div>
+            </div>
+            <div class="mb-2 d-flex justify-content-between">
+                <div class="col-3 pl-0">
+                    <label for="10-dollars" class="col-12">$10</label>
+                </div>
+                <div class="col-8 pr-0">
+                    <input class="col-12" type="number" id="10-dollars" name="10-dollars" min="0" value="">
+                </div>
+            </div>
+            <div class="mb-2 d-flex justify-content-between">
+                <div class="col-3 pl-0">
+                    <label for="5-dollars" class="col-12">$5</label>
+                </div>
+                <div class="col-8 pr-0">
+                    <input class="col-12" type="number" id="5-dollars" name="5-dollars" min="0" value="">
+                </div>
+            </div>
+            <div class="mb-2 d-flex justify-content-between">
+                <div class="col-3 pl-0">
+                    <label for="1-dollars" class="col-12">$1</label>
+                </div>
+                <div class="col-8 pr-0">
+                    <input class="col-12" type="number" id="1-dollars" name="1-dollars" min="0" value="">
+                </div>
+            </div>
+        </div>
+        <div class="bg-info my-2 p-1">
+            <div class="p-2 border border-white text-center text-white rem2">$0.00</div>
+        </div>
+    `;
+
+    container.append(backNextBtn("Go Back", "Next"));
+
+    return container;
+}
+
+function backNextBtn(back, next) {
+    let container = document.createElement("div");
+    container.classList.add("d-flex", "justify-content-between");
+    container.innerHTML = `
+        <div class="pl-0 col-6">
+            <button id="withdrawGoBack" class="col-12 btn btn-outline-primary">${back}</button>
+        </div>
+        <div class="pr-0 col-6">
+            <button id="withdrawProcess" class="col-12 btn btn-outline-primary">${next}</button>
+        </div>
+    `;
     return container;
 }
