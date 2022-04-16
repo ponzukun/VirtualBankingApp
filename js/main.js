5868e6f6aa07cd999def3b5277d82ce6d44fc67e
ponzukun <p1o1n1z1u@gmail.com>
2022-04-16 18:29:05 +0900

    [add namespace] config

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const config = {
+    initialForm : document.getElementById("initial-form"),
+    bankPage : document.getElementById("bank-page")
+}
+
+// console.log(config.initialForm);
+// console.log(config.bankPage);
